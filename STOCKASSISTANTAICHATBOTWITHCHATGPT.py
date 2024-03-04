37c39819d0a4fc45ea08b541395f6625f3fa09b7
Simthandi19 <83428018+Simthandi19@users.noreply.github.com>
2024-03-04 07:19:54 -0500

    Update STOCKASSISTANTAICHATBOTWITHCHATGPT.py

--- STOCKASSISTANTAICHATBOTWITHCHATGPT.py
+++ STOCKASSISTANTAICHATBOTWITHCHATGPT.py
@@ -264,4 +264,4 @@ if user_input:
             st.text(response_message['content'])
             st.session_state['messages'].append({'role': 'assistant', 'content': response_message['content']})
     except Exception as e:
-        st.text('Error occurred', e)
+        print(e)
