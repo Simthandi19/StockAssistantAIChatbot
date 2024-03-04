06720b8f03c60aac4654718d97f3f2a94196ea04
Simthandi19 <83428018+Simthandi19@users.noreply.github.com>
2024-03-04 14:09:17 +0000

    Update STOCKASSISTANTAICHATBOTWITHCHATGPT.py

--- STOCKASSISTANTAICHATBOTWITHCHATGPT.py
+++ STOCKASSISTANTAICHATBOTWITHCHATGPT.py
@@ -5,6 +5,7 @@
 
 import json
 import openai
+from openai import OpenAI
 import pandas as pd
 import matplotlib.pyplot as plt
 import streamlit as st
@@ -12,9 +13,19 @@ import yfinance as yf
 import os
 import subprocess
 
+client = OpenAI()
 
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
 
-openai.api_key=open('API_KEY.py', 'r').read()
+print(completion.choices[0].message)
+
+openai.api_key="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"
 
 
 def get_stock_price(ticker):
